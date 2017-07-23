2ee34641373d88730d2c1a87589493a6ccc730d4
prime23 <danzaljohnson@gmail.com>
2017-07-23 17:48:56 -0600

    Added color and now import library hexcolor

--- iconGenerator.go
+++ iconGenerator.go
@@ -3,20 +3,28 @@ package main
 import (
     "fmt"
     "flag"
+
+    "github.com/prime23/hexcolor"
 )
 
 func main() {
-    fmt.Println("--- Icon Generator ---")
 
     var bkgcString string
-    // var fgString string
+    var fgString string
     // var height int
     var width int
     // var outFile string
 
     flag.IntVar(&width, "width", 1024, "width to be used when generating icon")
     flag.StringVar(&bkgcString, "background", "f0f0f0", "background color of the icon in form of a hex string. e.x. 2ae9df")
+    flag.StringVar(&fgString, "color", "c75c93", "coloor of the icon")
     flag.Parse()
+
+    background, _ := hexcolor.HexRgb24BitToColor(bkgcString)
+    color, _ := hexcolor.HexRgb24BitToColor(fgString)
+
+    fmt.Println("--- Icon Generator ---")
     fmt.Printf("width: %d\n", width)
-    fmt.Printf("Color: %s\n", bkgcString)
+    fmt.Printf("Background: %t\n", background)
+    fmt.Printf("Color: %t\n", color)
 }
