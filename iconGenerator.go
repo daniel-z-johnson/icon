69048af1edaea9511b6449c5baaba676a7cc609c
prime23 <danzaljohnson@gmail.com>
2017-07-23 18:00:21 -0600

    Changed width to side

--- iconGenerator.go
+++ iconGenerator.go
@@ -11,11 +11,10 @@ func main() {
 
     var bkgcString string
     var fgString string
-    // var height int
-    var width int
+    var side int
     // var outFile string
 
-    flag.IntVar(&width, "width", 1024, "width to be used when generating icon")
+    flag.IntVar(&side, "side", 1024, "side to be used when generating icon")
     flag.StringVar(&bkgcString, "background", "f0f0f0", "background color of the icon in form of a hex string. e.x. 2ae9df")
     flag.StringVar(&fgString, "color", "c75c93", "coloor of the icon")
     flag.Parse()
@@ -24,7 +23,7 @@ func main() {
     color, _ := hexcolor.HexRgb24BitToColor(fgString)
 
     fmt.Println("--- Icon Generator ---")
-    fmt.Printf("width: %d\n", width)
+    fmt.Printf("side: %d\n", side)
     fmt.Printf("Background: %t\n", background)
     fmt.Printf("Color: %t\n", color)
 }
