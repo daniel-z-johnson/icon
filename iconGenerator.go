d01a66419a0c35e6b6d49640c6a6752e07ae6af2
prime23 <danzaljohnson@gmail.com>
2017-07-23 18:53:41 -0600

    add os lib and creating file for icon image

--- iconGenerator.go
+++ iconGenerator.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
     "fmt"
     "flag"
 
@@ -12,18 +13,21 @@ func main() {
     var bkgcString string
     var fgString string
     var side int
-    // var outFile string
+    var outFile string
 
     flag.IntVar(&side, "side", 1024, "side to be used when generating icon")
     flag.StringVar(&bkgcString, "background", "f0f0f0", "background color of the icon in form of a hex string. e.x. 2ae9df")
     flag.StringVar(&fgString, "color", "c75c93", "coloor of the icon")
+	flag.StringVar(&outFile, "file", "file.png", "name of file for icon file")
     flag.Parse()
 
     background, _ := hexcolor.HexRgb24BitToColor(bkgcString)
     color, _ := hexcolor.HexRgb24BitToColor(fgString)
+	f1, _ := os.OpenFile(outFile, os.O_CREATE | os.O_WRONLY, 0644)
 
     fmt.Println("--- Icon Generator ---")
     fmt.Printf("side: %d\n", side)
     fmt.Printf("Background: %t\n", background)
     fmt.Printf("Color: %t\n", color)
+    fmt.Printf("f1: %t\n", f1)
 }
