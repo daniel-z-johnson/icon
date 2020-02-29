00572687119be0f875befdd8080dd1016ec1ea9d
daniel-z-johnson <danzaljohnson@gmail.com>
2020-02-29 10:46:44 -0700

    adding .gitignore

--- iconGenerator.go
+++ iconGenerator.go
@@ -5,34 +5,58 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"image"
 	"image/color"
+	"image/draw"
+	"image/jpeg"
+	"image/png"
+	"os"
 )
 
+// icon will be 500 x 500
+// boarder of 10
 func main() {
 	fmt.Println("Icon Generator start")
 
-	hexColor := flag.String("background", "f0f0f0", "Should be a hex string, can include alpha value")
+	hexColor := flag.String("background", "a0a0a0", "Should be a hex string, can include alpha value")
 	flag.Parse()
 
 	fmt.Printf("Background color: '%s'\n", *hexColor)
 
-	backgroundColor, err := rgbaValueToColor(hexColor)
+	backgroundColor, err := hexToColor(hexColor)
 	if err != nil {
 		// there isn't much I can do here, anyway this is mostly just for me anyway
 		panic(err)
 	}
 	fmt.Printf("Using color %+v for the background\n", backgroundColor)
 
+	img := initialImage(backgroundColor)
+	img = changeImage(10, 10, 100, 100, color.RGBA{224, 29, 33, 255}, img)
+
+	// write the icon out
+	// playing with jpg and comparing against png
+	f1, err := os.Create("icon.png")
+	f2, err2 := os.Create("icon.jpg")
+	if err != nil || err2 != nil {
+		// can do nothing but panic, sorry
+		panic(err)
+	}
+	png.Encode(f1, img)
+	var opt jpeg.Options
+	opt.Quality = 83
+	jpeg.Encode(f2, img, &opt)
+
 }
 
-func rgbaValueToColor(hexColor *string) (color.Color, error) {
+func hexToColor(hexColor *string) (color.RGBA, error) {
+	nilRGBA := color.RGBA{0, 0, 0, 0}
 	rgbaArray, err := hex.DecodeString(*hexColor)
 	if err != nil {
-		return nil, err
+		return nilRGBA, err
 	}
 
 	if len(rgbaArray) != 3 && len(rgbaArray) != 4 {
-		return nil, errors.New("The hex value given was too big")
+		return nilRGBA, errors.New("The hex value given was too big")
 	}
 
 	if len(rgbaArray) == 3 {
@@ -44,4 +68,26 @@ func rgbaValueToColor(hexColor *string) (color.Color, error) {
 		B: rgbaArray[2],
 		A: rgbaArray[3],
 	}, nil
+}
+
+// initialImage icon for now will be 500 by 500
+func initialImage(bg color.RGBA) draw.Image {
+	img := image.NewRGBA(image.Rect(0, 0, 500, 500))
+
+	for x := 0; x < 500; x++ {
+		for y := 0; y < 500; y++ {
+			img.SetRGBA(x, y, bg)
+		}
+	}
+
+	return img
+}
+
+func changeImage(x0, y0, x1, y1 int, color color.RGBA, img draw.Image) draw.Image {
+	for x := x0; x < x1; x++ {
+		for y := y0; y < y1; y++ {
+			img.Set(x, y, color)
+		}
+	}
+	return img
 }
