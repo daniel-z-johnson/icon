ad1e087348ac29e28a68b4428b4950e7846abee9
Daniel Johnson <danzaljohnson@gmail.com>
2026-05-02 13:42:35 -0500

    fixed test

--- icon_test.go
+++ icon_test.go
@@ -67,7 +67,7 @@ func TestHexToColorInvalidLength(t *testing.T) {
 func TestHexToColorInvalidValue(t *testing.T) {
 	_, err := hexToColor("zzzzzz")
 
-	assert.ErrorContains(t, err, "invalid hex color \"zzzzzz\":")
+	assert.EqualError(t, err, "invalid hex color \"zzzzzz\": encoding/hex: invalid byte: U+007A 'z'")
 }
 
 func TestInitialImage(t *testing.T) {
